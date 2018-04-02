44c69f15346aa9d0af269188cfb429b9f377dc70
Bartosz Łaniewski <laniewski.bartozzz@gmail.com>
2018-04-02 15:29:43 +0200

    Bump version 1.1.2

--- dist/index.js
+++ dist/index.js
@@ -4,19 +4,17 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.Resources = undefined;
-exports.getResources = getResources;
+
+var _resources = require("./resources");
+
+Object.defineProperty(exports, "Resources", {
+  enumerable: true,
+  get: function get() {
+    return _interopRequireDefault(_resources).default;
+  }
+});
 
 var _path = require("path");
-
-var _path2 = _interopRequireDefault(_path);
-
-var _mergeDescriptors = require("merge-descriptors");
-
-var _mergeDescriptors2 = _interopRequireDefault(_mergeDescriptors);
-
-var _events = require("events");
-
-var _events2 = _interopRequireDefault(_events);
 
 var _parser = require("./parser");
 
@@ -25,48 +23,26 @@ var _parser2 = _interopRequireDefault(_parser);
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 /**
- * Default resources.
+ * Parses `.html` files for custom resources. Finds any resources loaded by an
+ * HTML file for you and emits an event with all the data needed. Supports
+ * Promises.
  *
- * @type    {object}
- */
-var Resources = exports.Resources = {
-  Scripts: require("./resources/scripts"),
-  Styles: require("./resources/styles"),
-  Images: require("./resources/images")
-};
-
-/**
- * Glues all components together.
+ * @param   {string}    file
+ * @param   {Object}    options
+ * @param   {string}    options.cwd
+ * @param   {Array}     options.resources
  *
- * @param   {string}    file            HTML to get resources from
- * @param   {object}    opts            Custom options
- * @param   {string}    opts.cwd        Current working directory
- * @param   {array}     opts.resources  Resources to parse the HTML file for
- *
- * @return  {object}
+ * @throws  {Error}
+ * @return  {Object}
  */
 function getResources(file) {
-  var opts = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
+  var options = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
 
-  if (_path2.default.extname(file) !== ".html") {
-    throw new Error("You must provide an .html file, not " + _path2.default.extname(file));
+  if ((0, _path.extname)(file) !== ".html") {
+    throw new Error("You must provide a .html file, not " + (0, _path.extname)(file));
   }
 
-  if (!opts.resources) {
-    opts.resources = [Resources.Scripts, Resources.Styles, Resources.Images];
-  }
-
-  if (!opts.cwd) {
-    opts.cwd = process.cwd();
-  }
-
-  var main = _path2.default.resolve(opts.cwd, file);
-  var base = _path2.default.parse(main).dir;
-  var parser = { file: file, base: base, main: main, opts: opts };
-
-  (0, _mergeDescriptors2.default)(parser, _events2.default.prototype, false);
-  (0, _mergeDescriptors2.default)(parser, _parser2.default, false);
-
-  // $FlowFixMe
-  return parser.search();
+  return new _parser2.default(file, options);
 }
+
+exports.default = getResources;
---
+++ dist/resources/index.js
@@ -0,0 +1,11 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = {
+  Images: require("./images"),
+  Scripts: require("./scripts"),
+  Styles: require("./styles")
+};
+module.exports = exports["default"];
